1992a75d05bcd5728f49da15e1255aa86c83cd89
ray-tao <taor740@newschool.edu>
2023-12-15 12:58:56 -0500

    minor fixes

--- 05-water/sketch.js
+++ 05-water/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(300, 450);
+    createCanvas(350, 500);
     noStroke();
 }
 
@@ -7,23 +7,23 @@ function draw() {
     background(18, 33, 36);
 
     fill(0);
-    rect(0, 0, 300, 300, 200, 200, 0, 0);
+    rect(0, 0, 350, 350, 200, 200, 0, 0);
 
     let hr = hour();
     let mappedHour = map(hr, 0, 60, 0, 300);
     fill(184, 138, 48);
-    circle(mappedHour, 300, 100);
+    circle(mappedHour, 350, 100);
 
     let min = minute();
     let mappedMin = map(min, 0, 60, 0, 300);
     fill(84, 102, 43);
-    circle(mappedMin, 300, 50);
+    circle(mappedMin, 350, 50);
 
     let sec = second();
     let mappedSec = map(sec, 0, 60, 0, 300);
     fill(150, 78, 2);
-    circle(mappedSec, 300, 25);
+    circle(mappedSec, 350, 25);
 
     fill(0, 50, 50);
-    rect(0, 300, 400, 200);
+    rect(0, 350, 400, 200);
 }
